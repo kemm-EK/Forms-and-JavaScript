b59577a86df0962b9ec404ee3951abc479b975d9
Keld Marple <kemm@ek.dk>
2026-04-08 17:26:38 +0200

    adjust js

--- js/variabler.js
+++ js/variabler.js
@@ -1,9 +1,17 @@
+// Functions
+function revealAdr() {
+  street.textContent = currentStreet;
+  postal.textContent = currentPostal;
+}
+
+// All my let variables
 let currentStreet = "Guldbergsgade 29N";
 let currentPostal = "2200 Nørrebro";
 
-document.querySelector("#adr-button").addEventListener("click", revealAdr);
+// All my element references
+const street = document.querySelector("#street");
+const postal = document.querySelector("#postal");
+const adrBtn = document.querySelector("#adr-button");
 
-function revealAdr() {
-  document.querySelector("#street").textContent = currentStreet;
-  document.querySelector("#postal").textContent = currentPostal;
-}
+// EventListeners
+adrBtn.addEventListener("click", revealAdr);
