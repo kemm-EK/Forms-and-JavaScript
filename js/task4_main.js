4a838b24569282c6ecf6821680854e224c223214
Keld Marple <kemm@ek.dk>
2026-04-09 17:27:21 +0200

    add submit

---
+++ js/task4_main.js
@@ -0,0 +1,20 @@
+// grib fat i form
+const form = document.querySelector("form");
+
+// stop default ved invalid
+form.addEventListener("invalid", cancelPopup, true);
+
+// formen submitter
+form.addEventListener("submit", submitForm);
+
+function cancelPopup(event) {
+  console.log("cancelPopup");
+  // console.log(event);
+  event.preventDefault();
+}
+
+function submitForm(event) {
+  console.log("submitForm");
+  event.preventDefault();
+  form.reset();
+}
