bac00ef97c8d1db252e2bdd5d86c991610b8eb63
Keld Marple <kemm@ek.dk>
2026-04-09 18:54:47 +0200

    adddataprint

--- js/task5_main.js
+++ js/task5_main.js
@@ -16,9 +16,27 @@ function cancelPopup(event) {
 function submitForm(event) {
   console.log("submitForm");
   event.preventDefault();
+  printdata();
   form.reset();
 }
 
 // ************************************************************
 // ny javascript
 // ************************************************************
+
+function printdata() {
+  console.log("printdata");
+
+  // have fat i data fra form
+  const data = new FormData(form);
+  const pet = data.getAll("pet");
+
+  document.querySelector("#output-firstname > span").textContent = data.get("firstname");
+  document.querySelector("#output-password> span").textContent = data.get("pw");
+  document.querySelector("#output-mail> span").textContent = data.get("mail");
+  document.querySelector("#output-age> span").textContent = data.get("num");
+
+  document.querySelector("#output-pet> span").textContent = pet.join(", ");
+
+  // Skrive data fra form ud i UI
+}
